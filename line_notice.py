f31de396cd8a7a9a52f914a3190192563553f9e7
taru <taruru1010@gmail.com>
2020-10-22 03:49:48 +0900

    コメントアウトで概要の追加と関数名の変更と諸々

--- line_notice.py
+++ line_notice.py
@@ -2,13 +2,19 @@ import requests
 import dotenv_loader
 
 
-def sendToLine(notification_message):
+# Lineにメッセージを送信する関数　送信したい文字列を引数に定義
+def send(send_message):
     line_notify_token = dotenv_loader.token
     line_notify_api = 'https://notify-api.line.me/api/notify'
+
+    # トークン情報をヘッダーに載せる
     headers = {'Authorization': f'Bearer {line_notify_token}'}
-    data = {'message': f'message: {notification_message}'}
+
+    data = {'message': send_message}
+    # 送信
     requests.post(line_notify_api, headers = headers, data = data)
+
 
 if __name__ == "__main__":
     text = "test"
-    sendToLine(text)
+    send(text)
