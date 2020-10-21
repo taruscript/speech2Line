05c8fd8d94d1a0829662de6b426f1405034b2ddd
taru <taruru1010@gmail.com>
2020-10-22 03:36:05 +0900

    モジュールで実行できるようにmainを追加

--- line_notice.py
+++ line_notice.py
@@ -2,12 +2,13 @@ import requests
 import dotenv_loader
 
 
-def send_line_notify(notification_message):
-    """
-    LINEに通知する
-    """
+def sendToLine(notification_message):
     line_notify_token = dotenv_loader.token
     line_notify_api = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {line_notify_token}'}
     data = {'message': f'message: {notification_message}'}
     requests.post(line_notify_api, headers = headers, data = data)
+
+if __name__ == "__main__":
+    text = "test"
+    sendToLine(text)
