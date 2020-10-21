63a779dd0a2848f74dc02211b9e85641f031f256
taru <taruru1010@gmail.com>
2020-10-22 04:54:30 +0900

    リfuckタリング

--- voice.py
+++ voice.py
@@ -1,53 +1,69 @@
 import speech_recognition as sr
  
 import numpy as np
-import pyaudio  #録音機能を使うためのライブラリ
-import wave     #wavファイルを扱うためのライブラリ
+import pyaudio
+import wave
  
 
-def voice_recode():
+
+class voiceFunctionsClass:
+    def __init__(self):
+        self.CHUNK = 1024
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 1
+        self.RATE = 48000
+        self.RECORD_SECONDS = 5
+        self.WAVE_OUTPUT_FILENAME ="recorded.wav"
+
+        self.p = pyaudio.PyAudio()
+
+        self.stream = self.p.open(format=self.FORMAT,
+                        channels=self.CHANNELS,
+                        rate=self.RATE,
+                        input=True,
+                        frames_per_buffer=self.CHUNK)
+
+    # 録音処理
+    def voice_recode(self):
+        print("＜＜＜録音中＞＞＞")
+
+        frames = []
+        calc = int(self.RATE / self.CHUNK * self.RECORD_SECONDS)
+        for i in range(0, calc):
+            data = self.stream.read(self.CHUNK)
+            buf = np.frombuffer(data, dtype="int16")
+            frames.append(b''.join(buf[::3]))
+        
+        print("＜＜＜録音終了＞＞＞")
+        # 録音保存　処理
+        self.voice_save(frames)
     
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 48000 # 録音は48kHz
-    RECORD_SECONDS = 5
-    WAVE_OUTPUT_FILENAME ="sample.wav"
 
-    p = pyaudio.PyAudio()
+    # 録音保存　処理
+    def voice_save(self, frames):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
 
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
+        wf = wave.open(self.WAVE_OUTPUT_FILENAME, 'wb')
+        wf.setnchannels(self.CHANNELS)
+        wf.setsampwidth(self.p.get_sample_size(self.FORMAT))
+        wf.setframerate(self.RATE / 3)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+    
 
-    print("* recording")
-
-    frames = []
-
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        buf = np.frombuffer(data, dtype="int16") # 読み込んだストリームデータを2byteのInt型のリストに分離
-        frames.append(b''.join(buf[::3])) # 記録するデータを1/3に間引いてリストを結合してフレームに追加
-
-    print("* done recording")
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE / 3) # ヘッダのサンプリングレートを16kHzにする
-    wf.writeframes(b''.join(frames))
-    wf.close()
+    #　音声認識
+    def voice_recognize(self):
+        print("音声認識を開始します") 
+        r = sr.Recognizer()
+        with sr.AudioFile(self.WAVE_OUTPUT_FILENAME) as source:
+            audio = r.record(source)
+        result = r.recognize_google(audio, language='ja-JP')
+        return result
 
 
-def voice_recognize(): 
-    r = sr.Recognizer()
-    with sr.AudioFile("sample.wav") as source:
-        audio = r.record(source)
-    result = r.recognize_google(audio, language='ja-JP')
-    return result
+if __name__ == "__main__":
+    voice = voiceFunctionsClass()
+    voice.voice_recode()
+    print(f"音声認識結果: {voice.voice_recognize()}")
